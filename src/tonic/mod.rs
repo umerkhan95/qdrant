401d9d4f846d3e071ce33dd4af5c97d09d0632be
Daniel Boros <dancixx@gmail.com>
2026-03-06 11:31:39 +0100

    feat: update qdrant tonic definition

--- src/tonic/mod.rs
+++ src/tonic/mod.rs
@@ -23,6 +23,7 @@ use ::api::grpc::qdrant::qdrant_internal_server::QdrantInternalServer;
 use ::api::grpc::qdrant::qdrant_server::{Qdrant, QdrantServer};
 use ::api::grpc::qdrant::shard_snapshots_server::ShardSnapshotsServer;
 use ::api::grpc::qdrant::snapshots_server::SnapshotsServer;
+use ::api::grpc::qdrant::storage_read_server::StorageReadServer;
 use ::api::grpc::qdrant::{HealthCheckReply, HealthCheckRequest};
 use ::api::rest::models::VersionInfo;
 use collection::operations::verification::new_unchecked_verification_pass;
@@ -48,6 +49,7 @@ use crate::tonic::api::points_api::PointsService;
 use crate::tonic::api::points_internal_api::PointsInternalService;
 use crate::tonic::api::qdrant_internal_api::QdrantInternalService;
 use crate::tonic::api::snapshots_api::{ShardSnapshotsService, SnapshotsService};
+use crate::tonic::api::storage_read_api::StorageReadService;
 
 #[derive(Default)]
 pub struct QdrantService {}
@@ -113,6 +115,7 @@ pub fn init(
         let collections_service = CollectionsService::new(dispatcher.clone());
         let points_service = PointsService::new(dispatcher.clone(), settings.service.clone());
         let snapshot_service = SnapshotsService::new(dispatcher.clone());
+        let storage_read_service = StorageReadService::new(dispatcher.clone());
 
         // Only advertise the public services. By default, all services in QDRANT_DESCRIPTOR_SET
         // will be advertised, so explicitly list the services to be included.
@@ -123,6 +126,7 @@ pub fn init(
             .with_service_name("qdrant.Snapshots")
             .with_service_name("qdrant.Qdrant")
             .with_service_name("grpc.health.v1.Health")
+            .with_service_name("qdrant.StorageRead")
             .build()
             .unwrap();
 
@@ -190,6 +194,12 @@ pub fn init(
             )
             .add_service(
                 HealthServer::new(health_service)
+                    .send_compressed(CompressionEncoding::Gzip)
+                    .accept_compressed(CompressionEncoding::Gzip)
+                    .max_decoding_message_size(usize::MAX),
+            )
+            .add_service(
+                StorageReadServer::new(storage_read_service)
                     .send_compressed(CompressionEncoding::Gzip)
                     .accept_compressed(CompressionEncoding::Gzip)
                     .max_decoding_message_size(usize::MAX),
