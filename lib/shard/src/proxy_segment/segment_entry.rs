4f50779c117a2b048b35d6939f77b6a731f67464
Ivan Boldyrev <ivan.boldyrev@qdrant.com>
2026-03-19 11:42:14 +0100

    Add `_concurrent` suffix to some methods

--- lib/shard/src/proxy_segment/segment_entry.rs
+++ lib/shard/src/proxy_segment/segment_entry.rs
@@ -650,13 +650,13 @@ impl StorageSegmentEntry for ProxySegment {
 }
 
 impl SegmentEntry for ProxySegment {
-    fn delete_point_mut(
+    fn delete_point(
         &mut self,
         op_num: SeqNumberType,
         point_id: PointIdType,
         hw_counter: &HardwareCounterCell,
     ) -> OperationResult<bool> {
-        NonAppendableSegmentEntry::delete_point(self, op_num, point_id, hw_counter)
+        NonAppendableSegmentEntry::delete_point_concurrent(self, op_num, point_id, hw_counter)
     }
 
     fn upsert_point(
@@ -743,11 +743,7 @@ impl SegmentEntry for ProxySegment {
         )))
     }
 
-    fn delete_field_index(
-        &mut self,
-        op_num: u64,
-        key: PayloadKeyTypeRef,
-    ) -> OperationResult<bool> {
+    fn delete_field_index(&mut self, op_num: u64, key: PayloadKeyTypeRef) -> OperationResult<bool> {
         if self.version() > op_num {
             return Ok(false);
         }
@@ -820,7 +816,7 @@ impl SegmentEntry for ProxySegment {
 }
 
 impl NonAppendableSegmentEntry for ProxySegment {
-    fn delete_point(
+    fn delete_point_concurrent(
         &mut self,
         op_num: SeqNumberType,
         point_id: PointIdType,
